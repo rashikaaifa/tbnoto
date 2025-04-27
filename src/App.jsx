11951739bfb982086a34f7d60d1824ce70cf159d
Rashika <rashikaaifariyanto2000@gmail.com>
2025-04-28 06:43:26 +0700

    [RAR] Add component navbar dan update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 import Homepage from './pages/Homepage';
 
-function App() {
+const AppContent = () => {
+  const location = useLocation();
+  const hideLayout = ['',].includes(location.pathname);
+
   return (
-    <Router>
+    <div className="overflow-x-hidden flex flex-col min-h-screen">
+      {!hideLayout && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
-}
+};
 
 export default App;
