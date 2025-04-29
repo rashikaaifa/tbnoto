524f12e1e417a3bee88b87624bc1acbf50072262
Pillows11 <pillowskun11@gmail.com>
2025-04-29 08:53:28 +0700

    feat [APS] : Add keranjang components & data dummy

---
+++ src/data/products.js
@@ -0,0 +1,83 @@
+// src/data/products.js
+export default [
+    {
+      id: 1,
+      name: 'TRIPLEK',
+      size: '2 meter x 80 cm',
+      quantity: 9,
+      price: 65000,
+      image: 'https://i1.wp.com/besemahpustaka.com/wp-content/uploads/2020/05/triplek.jpg?fit=770%2C482&ssl=1'
+    },
+    {
+      id: 2,
+      name: 'BESI',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 70000,
+      image: 'https://kpssteel.com/storage/2021/07/besi-assental-Distributor-besi-jakarta-KPS-Steel.jpg'
+    },
+    {
+      id: 3,
+      name: 'KASO',
+      size: '2 meter x 80 cm',
+      quantity: 7,
+      price: 70000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 4,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 5,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 6,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 7,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 8,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 9,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    },
+    {
+      id: 10,
+      name: 'PARALON',
+      size: '2 meter x 80 cm',
+      quantity: 6,
+      price: 60000,
+      image: 'https://www.superbangunjaya.com/wp-content/uploads/2020/11/1000004782-600x600.jpg'
+    }
+  ];
