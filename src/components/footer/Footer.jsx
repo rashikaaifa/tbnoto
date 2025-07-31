ccc04f20f36f846e530f1e37c7f7141ab34a8ef8
Rashika <rashikaaifariyanto2000@gmail.com>
2025-07-31 19:29:20 +0700

    Revert "fix [RAR]: menu di navbar dan footer"
    This reverts commit 9dc69555a36e468c69708e222a7d1a9dcfab9900, reversing
    changes made to ea6880e51f3e5cd2e64fbc4acfe244789e7157c4.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -45,6 +45,7 @@ const Footer = () => {
                         <li><a href="/keranjang" className="">Keranjang</a></li>
                         <li><a href="/riwayat" className="">Riwayat</a></li>
                         <li><a href="/bantuan" className="">Bantuan</a></li>
+                        <li><a href="/profil" className="">Profil</a></li>
                         </ul>
                     </div>
         
@@ -113,7 +114,7 @@ const Footer = () => {
                         </p>
                     </div>
                     <ul className="flex flex-wrap justify-center gap-4 p-2">
-                        {["Katalog", "Keranjang", "Riwayat", "Bantuan"].map((item, index) => (
+                        {["Kategori", "Katalog", "Keranjang", "Riwayat", "Bantuan", "Profil"].map((item, index) => (
                             <li key={index}>
                                 <a href="#" className="">{item}</a>
                             </li>
